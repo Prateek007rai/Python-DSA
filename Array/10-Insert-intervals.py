8b6a6ea8e689164f688d00638ae143c48dd2b994
Prateek Rai <prateekrai341036@gmail.com>
2026-04-04 12:52:02 +0530

    10-Insert-intervals

---
+++ Array/10-Insert-intervals.py
@@ -0,0 +1,18 @@
+# insert intervals
+
+def insert_intervals(intervals, new):
+    intervals.append(new)
+    intervals.sort()
+    n= len(intervals)
+    res = [intervals[0]]
+
+    for i in range(1, n):
+        last = res[-1]
+        if last[1] > intervals[i][0]:
+            last[1] = max(last[1], intervals[i][1])
+        else:
+            res.append(intervals[i])
+
+    return res
+
+print(insert_intervals([[1, 3], [6, 9]], [2, 5]))                     #[[1, 5], [6, 9]]

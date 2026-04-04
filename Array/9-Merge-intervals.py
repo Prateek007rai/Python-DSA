1df5133f5c1b1c122f61f9baa6eddb2835bf5678
Prateek Rai <prateekrai341036@gmail.com>
2026-04-04 12:46:52 +0530

    9-Merge-intervals

---
+++ Array/9-Merge-intervals.py
@@ -0,0 +1,18 @@
+# merge intervals
+
+def merge_intervals(intervals):
+    n = len(intervals)
+    intervals.sort()
+    res = [intervals[0]]
+
+    for i in range(1, len(intervals)):
+        last = res[-1]
+
+        if last[1] > intervals[i][0]:
+            last [1] = max(last[1], intervals[i][1])
+        else:
+            res.append(intervals[i])
+
+    return res
+            
+print(merge_intervals([[1, 3], [2, 6], [8, 16], [15, 18]]))

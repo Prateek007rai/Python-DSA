c9fc892eb0fe37db72efa3c4b040bf952e889423
Prateek Rai <prateekrai341036@gmail.com>
2026-04-11 20:29:31 +0530

    sorting of 0,1,2 with dutch national flag algo

--- Searching and Sorting/10-sort 0,1,2 .py
+++ Searching and Sorting/10-sort 0,1,2 .py
@@ -5,9 +5,25 @@
 
 # time: O(n), Space: O(1) - Dutch National Flag Algo
 
+def sort_colors(arr):
+    l = mid = 0
+    r = len(arr) - 1
 
+    while mid <= r:
 
+        if arr[mid] == 0:
+            arr[mid], arr[l] = arr[l], arr[mid]
+            l += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid], arr[r] = arr[r], arr[mid]
+            r -= 1
 
+    return arr
+
+print("Way 1 'Dutch National Flag' : ",sort_colors([2,0,2,0,1,1,0]))
 
 
 
@@ -22,4 +38,4 @@ def sort_nums(arr):
 
     return res
 
-print("Way 2 for sorting: ", sort_nums([2,0,2,0,1,1,0])) 
+print("Way 2 for sorting : ", sort_nums([2,0,2,0,1,1,0])) 

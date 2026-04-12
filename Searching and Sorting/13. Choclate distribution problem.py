c70540384526cd21bf52579bdc193543293f428f
Prateek Rai <prateekrai341036@gmail.com>
2026-04-12 16:00:09 +0530

    solved the choclate distribution problem

--- Searching and Sorting/13. Choclate distribution problem.py
+++ Searching and Sorting/13. Choclate distribution problem.py
@@ -1,6 +1,15 @@
 # Choclate Distribution Problem
-# i/p: [7,3,2,4,9,2,56], m = 3
+# i/p: [7,3,2,4,9,12,56], m = 3
 # o/p: 2
 
 def choclate_dist(arr, m):
-    
+    arr.sort()
+    res = float('inf')
+
+    for i in range(len(arr)-m+1):
+        diff = arr[i+m-1] - arr[i]
+        res = min(diff, res)
+   
+    return res
+
+print(choclate_dist([7,3,2,4,9,12,56], 3))

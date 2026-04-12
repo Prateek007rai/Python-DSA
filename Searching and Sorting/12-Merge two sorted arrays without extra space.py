df6f4259ad55fef15b6a4999c18a77040eaaba61
Prateek Rai <prateekrai341036@gmail.com>
2026-04-12 15:46:38 +0530

    solved the merge_arrays problem

--- Searching and Sorting/12-Merge two sorted arrays without extra space.py
+++ Searching and Sorting/12-Merge two sorted arrays without extra space.py
@@ -5,3 +5,20 @@
 # o/p: m=[1,2,3,5,8], n=[9,10,13,15]
 
 
+def merge_arrays(arr1, arr2):
+    l,r = len(arr1)-1, 0
+
+    while l >= 0 and r < len(arr2):
+        print(arr1[l], arr2[r])
+        if arr1[l] > arr2[r]:
+            arr1[l], arr2[r] = arr2[r], arr1[l]
+        r += 1
+        l -= 1   
+    
+    arr1.sort()
+    arr2.sort()
+
+    return arr1, arr2
+            
+
+print(merge_arrays([1,5,9,10,15], [2,3,8,13]))

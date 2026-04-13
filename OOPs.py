9ddc8021aa7971a0bb858a657a9146a3a8978fb0
Prateek Rai <prateekrai341036@gmail.com>
2026-04-13 18:44:30 +0530

    terminologies of oops

---
+++ OOPs.py
@@ -0,0 +1,7 @@
+# Class → Blueprint to create objects
+# Object → Instance of a class
+# Encapsulation → Hiding data using private variables
+# Abstraction → Showing only required details
+# Inheritance → Reusing properties from another class
+# Polymorphism → Same function, different behavior
+

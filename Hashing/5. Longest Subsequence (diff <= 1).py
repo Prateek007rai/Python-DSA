8e391b55ba352fdca029549f606ef424be6deb54
Prateek Rai <prateekrai341036@gmail.com>
2026-04-17 22:29:41 +0530

    Solved the longest subsequence problem

--- Hashing/5. Longest Subsequence (diff <= 1).py
+++ Hashing/5. Longest Subsequence (diff <= 1).py
@@ -2,3 +2,23 @@
 # i/p: [1,2,2,3,1,2]
 # o/p: 5
 
+def longest_sub(arr):
+    mp = {}
+    max_len = 0
+
+    # update counter in arr loop
+    for num in arr:
+        mp[num] = mp.get(num, 0) + 1
+
+    # loop in mp(hashing)
+    for num in mp:
+        curr = mp[num]
+        if (num+1) in mp:
+            curr = curr + mp[num+1]
+        
+        max_len = max(curr, max_len)
+    
+    return max_len
+    
+
+print(longest_sub([1,2,2,3,1,2]))

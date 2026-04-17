6fbaf11421d5008e339a466bebf77975229c0c1c
Prateek Rai <prateekrai341036@gmail.com>
2026-04-17 22:20:53 +0530

    Solved the count subarrya with xor

--- Hashing/6. Count Subarray with X-OR.py
+++ Hashing/6. Count Subarray with X-OR.py
@@ -1,3 +1,20 @@
 # Time: O(n), Space: O(n)
 # i/p: [4,2,2,6,4], k=6
 # o/p: 4
+
+
+def subarray_sum_using_xor(arr, k):
+    mp = {0:1}
+    count = 0
+    xr = 0                  #running X-OR
+
+    for num in arr:
+        xr = xr ^ num
+        if (xr ^ k) in mp:
+            count = count + mp[xr ^ k]
+        
+        mp[xr] = mp.get(xr, 0) + 1
+    
+    return count
+
+print(subarray_sum_using_xor([4,2,2,6,4], 6))

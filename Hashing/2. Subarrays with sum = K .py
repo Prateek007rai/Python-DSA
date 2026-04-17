03cfab76e192966c98e7250bfd4aee840c96b54f
Prateek Rai <prateekrai341036@gmail.com>
2026-04-17 22:13:53 +0530

    solved the subarray with sum = k problem

--- Hashing/2. Subarrays with sum = K .py
+++ Hashing/2. Subarrays with sum = K .py
@@ -2,3 +2,18 @@
 # i/p: [1,1,1], K=2
 # o/p: 2
 
+def subarray_sum(arr, k):
+    count = 0
+    prefix = 0            #running sum
+    mp = {0: 1}           #base condition for sum = 0
+
+    for num in arr:
+        prefix = prefix + num
+
+        if (prefix-k) in mp:
+            count = count + mp[(prefix-k)]
+        
+        mp[prefix] = mp.get(prefix, 0) + 1
+    return count
+
+print(subarray_sum([1,1,1], 2))

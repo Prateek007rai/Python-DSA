6868230241defaa9ab4fc51ee5347bf87ec1ad10
Prateek Rai <prateekrai341036@gmail.com>
2026-04-17 22:47:40 +0530

    reversed ll

--- Linked List/1. Reverse LL.py
+++ Linked List/1. Reverse LL.py
@@ -1,5 +1,19 @@
+# 3 Pointer technique
 # Time: O(n), Space: O(1)
 
 # i/p: 1->2->3->4
 # o/p: 4->3->2->1
 
+
+def reverse_list(head):
+    prev = None
+    curr = head
+    
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    
+    return prev
+    

dac07b5af4ec103d56da1bd18f49a94fc416ec8d
Prateek Rai <prateekrai341036@gmail.com>
2026-04-18 16:03:25 +0530

    solved the merge two sorted LL into the sorted one

--- Linked List/4. Merge two sortd LL.py
+++ Linked List/4. Merge two sortd LL.py
@@ -4,3 +4,21 @@
 # i/p: 1->3->5, 2->4->6
 # o/p: 1->2->3->4->5->6
 
+def merge_two_LL(list1, list2):
+
+    dummy= ListNode(0)
+    curr = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            curr.next = list1
+            list1 = list1.next
+        else:
+            curr.next = list2
+            list2 = list2.next
+        curr = curr.next
+    
+    # if left in any of LL
+    curr.next = list1 if list1 else list2
+
+    return dummy.next

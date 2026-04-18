eb7e756c613b09487292327efc82053781d7b3a8
Prateek Rai <prateekrai341036@gmail.com>
2026-04-18 15:46:13 +0530

    solved the remove nth node form the end

--- Linked List/3. Remove Nth node from end.py
+++ Linked List/3. Remove Nth node from end.py
@@ -3,3 +3,22 @@
 # i/p: 1->2->3->4->5, n= 2
 # o/p: 1->2->3->5
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def remove_nth(head, n):
+    dummy = ListNode(0)                   # Handle head removal
+    dummy.next = head
+    slow = fast = dummy
+
+    for _ in range(n + 1):                      # Create n+1 gap
+        fast = fast.next
+
+    while fast:                                 # Move slider
+        slow = slow.next
+        fast = fast.next
+
+    slow.next = slow.next.next                  # Delete target
+    return dummy.next                           # Return new head

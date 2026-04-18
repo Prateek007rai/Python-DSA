034c05e3bf6d0f3b211b82259793580d55aeadc2
Prateek Rai <prateekrai341036@gmail.com>
2026-04-18 16:28:36 +0530

    solved the merge K sorted LL

--- Linked List/5. Merge K sorted list.py
+++ Linked List/5. Merge K sorted list.py
@@ -3,3 +3,24 @@
 # i/p: [1->4->5, 1->3->4, 2->6]
 # o/p: [1->1->2->3->4->4->5->6]
 
+import heapq
+
+def merge_lls(lists):
+    dummy = ListNode(0)
+    curr = dummy
+    heap = []
+
+    # put each first node (head) in min-heap
+    for i in range(len(lists)):
+        heapq.heappush(heap, (list[i].val, i, list[i]))
+
+    # merge and store in dummy
+    while heap:
+        val, i , node = heapq.heappop(heap)
+        curr.next = node
+        curr = curr.next
+
+        if node.next:
+            heapq.heappush(heap, (node.next.val, i, node.next))
+    
+    return dummy.next

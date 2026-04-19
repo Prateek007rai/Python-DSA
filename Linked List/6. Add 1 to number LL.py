46dd050bc66030815ad0e75a970c2b72d64c0538
Prateek Rai <prateekrai341036@gmail.com>
2026-04-19 20:53:47 +0530

    solved the add 1 number to ll num problem

--- Linked List/6. Add 1 to number LL.py
+++ Linked List/6. Add 1 to number LL.py
@@ -2,3 +2,23 @@
 
 # i/p: 1->9->9
 # o/p: 2->0->0
+
+def add_one(head):
+     
+    head = reverse_list(head)
+    curr = head
+    carry = 1
+
+    while curr:
+        curr.val = curr.val + carry
+        carry = curr.val // 10                    # 10//10 = 1
+        curr.val = curr.val % 10                  # 10 % 10 = 0
+
+        if carry == 0:
+            break
+
+        if curr.next == None:
+            curr.next = ListNode(0)               # create  a new list node
+        curr = curr.next
+
+    return reverse_list(head)

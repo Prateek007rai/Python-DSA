833c7fed07bc23ac0f67dd988e58a689a02bbeba
Prateek Rai <prateekrai341036@gmail.com>
2026-04-19 23:11:58 +0530

    solved [Practice] longest substring problem

--- test.py
+++ test.py
@@ -96,3 +96,25 @@ def transpose(arr):
     return res
     
 print("Transpose of matrix: ", transpose([[1,2], [3,4], [5,6]]))
+
+
+# Longest substring without repeating char
+def long_sub(s):
+    max_len = 0
+    seen = set()
+    l = 0
+
+    for r in range(len(s)):
+
+        # shrink the sliding window
+        while s[r] in seen:
+            seen.remove(s[l])
+            l += 1
+
+        seen.add(s[r])
+
+        max_len = max(r-l+1, max_len)            #max len or sliding window size(r-l+1)
+    
+    return max_len
+
+print("Longest substring without repeating char: ", long_sub("abcabcbb"))

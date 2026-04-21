bc7b13898621f94d4a00dc30c03cf3438264f35e
Prateek Rai <prateekrai341036@gmail.com>
2026-04-21 16:45:28 +0530

    solved the rectangle histogram issue

--- Stack, queue and de-queue/3. Largest Rectangle in histogram.py
+++ Stack, queue and de-queue/3. Largest Rectangle in histogram.py
@@ -2,3 +2,22 @@
 # i/p: [2,1,5,6,2,3]
 # o/p: 10
 
+def largest_area(heights):
+    stack = []
+    max_area = 0
+
+    for i in range(len(heights)+1):
+        if i == len(heights):
+            h = 0
+        else:
+            h = heights[i]
+        
+        while stack and h < heights[stack[-1]]:
+            height = heights[stack.pop()]
+            width = i if not stack else i - stack[-1] - 1
+            max_area = max(max_area, height * width)
+        
+        stack.append(i)
+    return max_area
+
+print(largest_area([2,1,5,6,2,3]))

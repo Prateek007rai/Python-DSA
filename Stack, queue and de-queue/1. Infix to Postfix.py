4b23c62c2dddf890a2ce7ff2793cfbe4fa9d192d
Prateek Rai <prateekrai341036@gmail.com>
2026-04-21 16:06:45 +0530

    solved - infix to postfix conversion

--- Stack, queue and de-queue/1. Infix to Postfix.py
+++ Stack, queue and de-queue/1. Infix to Postfix.py
@@ -3,3 +3,31 @@
 # i/p: (A+B)*C
 # o/p: AB+C*
 
+def infix_to_postfix(s):
+    res = ''
+    stack = []
+    prec = {'(': 0,'+': 1, '-': 1, '/': 2, '*': 2, '^': 3}
+
+    for ch in s:
+        if ch.isalnum():
+            res = res + ch
+        elif ch == '(':
+            stack.append(ch)
+        elif ch == ')':
+            # pop until meet '('
+            while stack and stack[-1] != '(':
+                res = res + stack.pop()
+            stack.pop() 
+        else:
+            # insert operators, check if in stack there is high prec operator present or not
+            while stack and stack != '(' and prec[ch] <= prec[stack[-1]]:
+                res = res + stack.pop()
+            stack.append(ch)
+
+    # now pop stack until it is empty
+    while stack:
+        res = res + stack.pop()
+    return res
+
+print(infix_to_postfix("(A+B)*C"))
+

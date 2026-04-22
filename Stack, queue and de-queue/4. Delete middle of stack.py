f0f83b72891e8edaf0bb768438f15c9bae766fb5
Prateek Rai <prateekrai341036@gmail.com>
2026-04-22 16:31:43 +0530

    Solved the delete from the middel of the stack

--- Stack, queue and de-queue/4. Delete middle of stack.py
+++ Stack, queue and de-queue/4. Delete middle of stack.py
@@ -3,3 +3,15 @@
 # i/p: [1,2,3,4,5], n = 3 
 # o/p: [1,2,4,5]
 
+def delete_middle(stack, n):
+    if n == 1:
+        stack.pop()
+        return
+    
+    temp = stack.pop()
+    delete_middle(stack, n-1)
+    stack.append(temp)
+
+    return stack
+
+print(delete_middle([1,2,3,4,5], 3))

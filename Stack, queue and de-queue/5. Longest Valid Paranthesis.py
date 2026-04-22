f1054f37b655b00db2defd45374f19bd0627c879
Prateek Rai <prateekrai341036@gmail.com>
2026-04-22 16:41:35 +0530

    solved the problem of longest valid paranthesis

--- Stack, queue and de-queue/5. Longest Valid Paranthesis.py
+++ Stack, queue and de-queue/5. Longest Valid Paranthesis.py
@@ -3,3 +3,20 @@
 # i/p: ")()())"
 # o/p: 4
 
+def long_paranthesis(s):
+    stack = [-1]
+    res = 0
+
+    for i in range(len(s)):
+        print(i,"for", stack)
+        if s[i] == '(':
+            stack.append(i)
+        else:
+            stack.pop()
+            if not stack:
+                stack.append(i)
+            else:
+                res = max(res, i - stack[-1])
+    return res
+
+print(long_paranthesis(")()())"))

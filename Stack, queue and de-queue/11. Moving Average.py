f0ee36322f5c4f30b147b535e992d18e145ddb06
Prateek Rai <prateekrai341036@gmail.com>
2026-04-23 23:16:42 +0530

    Solve - moving average problem

---
+++ Stack, queue and de-queue/11. Moving Average.py
@@ -0,0 +1,4 @@
+# Goal: Keep average of the last k numbers.
+# Time: O(1) for any size of list, that s why it is better than normal list
+
+# Input: [1, 10, 3, 5], k=3 -> Output: [1.0, 5.5, 4.66, 6.0]

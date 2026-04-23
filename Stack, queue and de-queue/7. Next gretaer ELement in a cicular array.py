027f597e6bd1c66f2dc5dd33ef87565092b58fc2
Prateek Rai <prateekrai341036@gmail.com>
2026-04-23 16:51:55 +0530

    Solved the next greater element in circular array

--- Stack, queue and de-queue/7. Next gretaer ELement in a cicular array.py
+++ Stack, queue and de-queue/7. Next gretaer ELement in a cicular array.py
@@ -2,3 +2,22 @@
 # i/p: [1,2,1]
 # o/p: [2,-1,2]
 
+def NGE_circular_array(arr):
+    n = len(arr)
+    res = [-1] * n
+    stack = []
+    
+    for i in range(2*n -1, -1, -1):
+        while stack and stack[-1] <= arr[i % n]:
+            stack.pop()
+         
+        if i<n and stack:
+            res [i] = stack[-1]
+        
+        stack.append(arr[i % n])
+
+    
+    return res
+    
+   
+print(NGE_circular_array([1,2,1]))

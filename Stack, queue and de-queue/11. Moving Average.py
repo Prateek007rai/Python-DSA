ea319fe3110bbf901d3e0f7c29ab6f939b7d1426
Prateek Rai <prateekrai341036@gmail.com>
2026-04-24 19:42:02 +0530

    solved - moving avg problem in O(1)

--- Stack, queue and de-queue/11. Moving Average.py
+++ Stack, queue and de-queue/11. Moving Average.py
@@ -2,3 +2,22 @@
 # Time: O(1) for any size of list, that s why it is better than normal list
 
 # Input: [1, 10, 3, 5], k=3 -> Output: [1.0, 5.5, 4.66, 6.0]
+
+from collections import deque
+
+def moving_avg(nums, k):
+    avgs = []
+    q = deque()
+    curr_sum = 0
+
+    for num in nums:
+        if len(q) == k:
+            curr_sum = curr_sum - q.popleft()
+        
+        curr_sum = curr_sum + num
+        q.append(num)
+        avgs.append(curr_sum/len(q))
+
+    return avgs
+
+print(moving_avg([1, 10, 3, 5], 3))

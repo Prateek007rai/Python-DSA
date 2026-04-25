8dd87c92a33138d8b770b5d2aa636c885ccbcdaa
Prateek Rai <prateekrai341036@gmail.com>
2026-04-25 19:46:51 +0530

    solved the flip tree problem in O(n)

--- Trees/3. Flip Binary Tree.py
+++ Trees/3. Flip Binary Tree.py
@@ -1,2 +1,15 @@
 # Time: O(n), Space: O(n)
 # Idea: Do Swap -> the recurse
+
+
+def flip_tree(root):
+
+    if not root:
+        return None
+    
+    root.left, root.right  =  root.right, root.left
+
+    flip_tree(root.left)
+    flip_tree(root.right)
+
+    return root

8355697fe1673a75ea10358f86e72e4ba2688301
Prateek Rai <prateekrai341036@gmail.com>
2026-04-26 16:59:25 +0530

    solved - serialize and deserialize problem

--- Trees/6. Serialize and Deserialize Binary Tree.py
+++ Trees/6. Serialize and Deserialize Binary Tree.py
@@ -3,5 +3,39 @@
 # i/p: 1-> 2,3 and 3-> 4,5
 # o/p: 1,2,null,null,3,4,null,null,5,null,null
 
+# Serialize, Tree -> String
+def serialize(root):
+    res = []
+    
+    def dfs(node):
+        if not node:
+            res.append("null")
+            return 
+        res.append(str(node.val))
 
+        dfs(node.left)
+        dfs(node.right)
 
+    dfs(root)
+    return ",".join(res)    
+
+# De-serialize, String -> Tree
+def deserialize(data):
+    strData = data.split(",")
+    i = 0
+    
+    def dfs():
+        nonlocal i
+        if strData[i] == "null":
+            i += 1
+            return None
+        node = TreeNode(int(strData[i]))
+        i += 1
+
+        node.left = dfs()
+        node.right = dfs()
+
+        return node
+
+    return dfs()
+    

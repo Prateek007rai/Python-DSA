61129844d68007df47087c0aecd2774dc817e526
Prateek Rai <prateekrai341036@gmail.com>
2026-04-26 17:10:50 +0530

    solved - diameter of binary tree

--- Trees/7. Diameter of tree.py
+++ Trees/7. Diameter of tree.py
@@ -2,3 +2,19 @@
 # o/p: output is int, ex: 3 (edge, path: 4->2->1->3, edge is 3)
 
 def max_diameter(root):
+    max_dia = 0
+
+    def dfs(node):
+        nonlocal max_dia
+
+        if not node:
+            return 0
+        left = dfs(node.left)
+        right = dfs(node.right)
+
+        max_dia = max(max_dia, left + right)
+
+        return 1 + max(left, right)
+
+    dfs(root)
+    return max_dia

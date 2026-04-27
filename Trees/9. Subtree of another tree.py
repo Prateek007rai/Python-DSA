b79d28f0a0d69f839bccf09714caada7ad593a12
Prateek Rai <prateekrai341036@gmail.com>
2026-04-27 20:47:26 +0530

    solved - subtree of tree problem

--- Trees/9. Subtree of another tree.py
+++ Trees/9. Subtree of another tree.py
@@ -2,3 +2,22 @@
 # i/p: root, subroot
 # o/p: boolean
 
+def is_same(a,b):
+    if not a and not b:
+        return True
+    
+    if not a or not b:
+        return False
+    
+    return (a.val == b.val and is_same(a.left, b.left) and is_same(a.right, b.right))
+    
+
+def is_subtree(root, subroot):
+    if not root:
+        return False
+    
+    if is_same(root, subroot):
+        return True
+    
+    return (is_subtree(root.left, subroot) or
+            is_subtree(root.right, subroot))

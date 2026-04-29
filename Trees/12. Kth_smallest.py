54aa10035871f2fed926673a09f8a5c67ffbd2b6
Prateek Rai <prateekrai341036@gmail.com>
2026-04-29 21:32:46 +0530

    Solved - the kth smallest ele

--- Trees/12. Kth_smallest.py
+++ Trees/12. Kth_smallest.py
@@ -1,2 +1,18 @@
 # Time: O(n), Space: O(h)
 
+def kth_smallest(root, k):
+    stack = []
+
+    while True:
+
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+
+        if k == 0:
+            return root.val
+        
+        root  = root.right
+        

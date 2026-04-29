946467f2c250d504730c1ab4f629e9db55e02cc4
Prateek Rai <Prateekrai341036@gmail.com>
2026-04-29 23:24:13 +0530

    solved - the LCA BST

--- Trees/13. LCA in BST.py
+++ Trees/13. LCA in BST.py
@@ -4,3 +4,12 @@
 # both < root -> move left
 # else: return root
  
+
+def lca_bst(root, p, q):
+    while root:
+        if p.val < root.val and q.val < root.val:
+            root = root.left
+        elif p.val > root.val and q.val > root.val:
+            root = root.right
+        else:
+            return root

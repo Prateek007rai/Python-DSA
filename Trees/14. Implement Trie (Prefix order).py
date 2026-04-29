383bf34a704a4dc93b23ee2e70c0ecba5280ce83
Prateek Rai <Prateekrai341036@gmail.com>
2026-04-29 23:56:26 +0530

    Made trie node

---
+++ Trees/14. Implement Trie (Prefix order).py
@@ -0,0 +1,16 @@
+# Time: O(n), Space: O(n)
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+
+        for ch in word 
+

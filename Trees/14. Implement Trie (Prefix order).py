aef0c2e0168c3951fcfed8f78c302472eb168a67
Prateek Rai <Prateekrai341036@gmail.com>
2026-04-30 00:14:31 +0530

    solved = trie node

--- Trees/14. Implement Trie (Prefix order).py
+++ Trees/14. Implement Trie (Prefix order).py
@@ -13,5 +13,24 @@ class Trie:
         node = self.root
 
         for ch in word:
-            if
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.end = True
 
+    def search(self, word):
+        node = self.root
+
+        for ch in word:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return node.end
+    
+    def startsWith(self, prefix):
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return True

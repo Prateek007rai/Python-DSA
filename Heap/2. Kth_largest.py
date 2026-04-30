bb646a1d638e4b343a3d210d9806d0f9bb331c24
Prateek Rai <Prateekrai341036@gmail.com>
2026-04-30 21:55:15 +0530

    solved - the kth largest element problem

--- Heap/2. Kth_largest.py
+++ Heap/2. Kth_largest.py
@@ -3,3 +3,17 @@
 # i/p: [3,2,1,5,6,4], k = 2
 # o/p: 5
 
+import heapq
+
+def kth_largest(arr,k):
+    heap = []
+
+    for num in arr:
+
+        heapq.heappush(heap,num)
+
+        if len(heap) > k:
+            heapq.heappop(heap)
+    
+    return heap[0]
+

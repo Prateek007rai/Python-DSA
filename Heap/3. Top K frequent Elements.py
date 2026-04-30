e38485ba6a75829835e4031d9d6f3e527041e00f
Prateek Rai <Prateekrai341036@gmail.com>
2026-04-30 22:18:20 +0530

    Solved - top k freq elements

--- Heap/3. Top K frequent Elements.py
+++ Heap/3. Top K frequent Elements.py
@@ -2,3 +2,27 @@
 
 # i/p: [1,1,1,2,2,3], k = 2
 # o/p: [1,2]
+
+import heapq
+from collections import Counter
+
+def top_k_freq(arr, k):
+    freq = Counter(arr)
+    heap = []
+    res = []
+
+    print(freq)
+    for num,count in freq.items():
+        heapq.heappush(heap, [num,count])
+
+        if len(heap) > k:
+            heapq.heappop(heap)
+    print(heap)
+
+    for count, num in heap:
+        res.append(num)
+
+    return res
+
+
+print(top_k_freq([1,1,1,2,2,3], 2))

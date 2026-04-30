67c109161eb6e1ca24b2a829c46cd3f8bbd57fb3
Prateek Rai <Prateekrai341036@gmail.com>
2026-04-30 22:47:04 +0530

    solved the merge_k_sorted lined list problem

--- Heap/4. Merge K sorted lists.py
+++ Heap/4. Merge K sorted lists.py
@@ -3,3 +3,25 @@
 # i/p: [linked_list_0, linked_list_1,linked_list_2]  
 # o/p: [single linked list]
 
+import heapq
+
+def merge_k_sorted_lls(lists):
+    heap = []
+
+    for i in range(len(lists)):
+        if lists[i]:
+            heapq.heappush(heap, (lists[i].val, i, lists[i]))
+    
+    dummy = ListNode(0)
+    curr = dummy
+
+    while heap:
+        val, i ,node = heapq.heappop(heap)
+        
+        curr.next = node
+        curr = curr.next
+
+        if node.next:
+            heapq.heappush(heap, (node.next.val, i, node.next))
+    
+    return dummy.next

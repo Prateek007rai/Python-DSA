615feb0dcf39739ea48085475fb9c602cb1543b6
Prateek Rai <Prateekrai341036@gmail.com>
2026-05-01 22:08:59 +0530

    solved - find median from data stream

--- Heap/5. Find Median from data stream.py
+++ Heap/5. Find Median from data stream.py
@@ -2,3 +2,36 @@
 
 # i/p: [5,15,1,3]
 # o/p: 5 10 5 4
+
+import heapq
+ 
+class MedianFind:
+
+    def __init__(self):
+        self.large = []            #min heap
+        self.small = []            #Max heap (store -ve value)
+    
+    def addNum(self, num):
+        # directly push into any one of the heap, lets say small heap
+        heapq.heappush(self.small, -1*num)
+
+        # check for balance, small should contain ele < ele of large
+        if self.small and self.large and (-1 * self.small[0] >= self.large[0]):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        # check condition for length (diff should not be greater then 1)
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        # conditon to check weather large heap's length is not greater than small
+        if len(self.large) > len(self.small):
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self):
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        return (-1*self.small[0] + self.large[0]) / 2
+    

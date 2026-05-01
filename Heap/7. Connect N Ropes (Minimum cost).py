f50e5718a61c5bb1fa0a5aec379506fd34c2bd55
Prateek Rai <Prateekrai341036@gmail.com>
2026-05-01 19:57:07 +0530

    solved - Min cost for connect 3 ropes

--- Heap/7. Connect N Ropes (Minimum cost).py
+++ Heap/7. Connect N Ropes (Minimum cost).py
@@ -2,3 +2,22 @@
 
 # i/p: [4,3,2,6]
 # o/p: 29
+
+import heapq
+
+def min_cost(arr):
+    heapq.heapify(arr)
+    cost = 0
+    total = 0
+    while len(arr) > 1:
+        first= heapq.heappop(arr)
+        second= heapq.heappop(arr)
+
+        total = first + second
+        cost  = total + cost
+
+        heapq.heappush(arr, total)
+    
+    return cost
+
+print(min_cost([4,3,2,6]))

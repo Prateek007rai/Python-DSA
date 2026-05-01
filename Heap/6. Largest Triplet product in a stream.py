d9a70ff858becac02548e766e00f10d05bf6c6d6
Prateek Rai <Prateekrai341036@gmail.com>
2026-05-01 19:47:45 +0530

    Solved - largest triplet product

--- Heap/6. Largest Triplet product in a stream.py
+++ Heap/6. Largest Triplet product in a stream.py
@@ -2,3 +2,27 @@
 # 
 # i/p: [1,2,3,4,5]  
 # o/p: [-1,-1,6,24,60]
+
+import heapq
+
+def larg_trip_prod(arr):
+    res = []
+    heap = []
+
+    for num in arr:
+        heapq.heappush(heap, num)
+
+        if len(heap) > 3:
+            heapq.heappop(heap)
+        
+        if len(heap) < 3:
+            res.append(-1)
+        else:
+            product = 1
+            for i in heap:
+                product *= i
+            res.append(product)
+        
+    return res
+
+print(larg_trip_prod([1,2,3,4,5]))

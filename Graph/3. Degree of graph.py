047ffc0769133cc020a0cc8ff72f97df0164eea7
Prateek Rai <Prateekrai341036@gmail.com>
2026-05-03 00:30:53 +0530

    solved - degree of graph

--- Graph/3. Degree of graph.py
+++ Graph/3. Degree of graph.py
@@ -1,3 +1,14 @@
 # Time: O(E), Space: O(V)
 # i/p: n=3, edges = [[0,1], [0,2], [1,2]]
 # o/p: [2,2,2]
+
+def deg_graph(n, edges):
+    deg = [0] * n
+    print(deg)
+
+    for u,v in edges:
+        deg[u] += 1
+        deg[v] += 1
+    return deg
+
+print(deg_graph(3, [[0,1], [0,2], [1,2]]))

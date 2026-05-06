3e57a9f3ead34be549a47cc7dec86e5a03100f18
Prateek Rai <Prateekrai341036@gmail.com>
2026-05-06 21:12:45 +0530

    Solve - Dijikstra algorithm

--- Graph/6. Dijikstra.py
+++ Graph/6. Dijikstra.py
@@ -1,2 +1,4 @@
 # Find Shortest path from the source to all nodes, but not for -ve weights.
 # Time: O(E * log V), Space: O(V)
+
+# i/p: n=4, edges=[(0,1,4), (0,2,1), (2,1,2), (1,3,1), (2,3,5)], src=0

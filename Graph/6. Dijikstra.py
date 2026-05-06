78b1699d1c0b33573f7b36242c78fbb0fc91830b
Prateek Rai <Prateekrai341036@gmail.com>
2026-05-06 21:32:08 +0530

    Solved - Dijikstra algorithm

--- Graph/6. Dijikstra.py
+++ Graph/6. Dijikstra.py
@@ -3,3 +3,33 @@
 
 # i/p: n=4, edges=[(0,1,4), (0,2,1), (2,1,2), (1,3,1), (2,3,5)], src=0
 # o/p: [0,3,1,4] <--- these are the wt. of path
+
+
+import heapq
+
+def dijikstra(n,edges,src):
+    graph = {i: [] for i in range(n)}
+
+    for u,v,w in edges:
+        graph[u].append((v,w))
+        graph[v].append((u,w))
+
+    dist = [float('inf')] * n
+    dist[src] = 0 
+
+    heap=[(0,src)]
+
+    while heap:
+        distance, node = heapq.heappop(heap)
+
+        if distance > dist[node]:
+            continue
+
+        for neighbour, wt in graph[node]:
+
+            if distance + wt < dist[neighbour]:
+                dist[neighbour] = distance + wt
+                heapq.heappush(heap, (distance + wt, neighbour))
+
+    return dist
+

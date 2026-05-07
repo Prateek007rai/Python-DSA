d7ba2ccc230936eb9d7f238235179e15abf81c28
Prateek Rai <110284131+Prateek007rai@users.noreply.github.com>
2026-05-07 23:19:25 +0530

    Update 3. Degree of graph.py

--- Graph/3. Degree of graph.py
+++ Graph/3. Degree of graph.py
@@ -2,13 +2,3 @@
 # i/p: n=3, edges = [[0,1], [0,2], [1,2]]
 # o/p: [2,2,2]
 
-def deg_graph(n, edges):
-    deg = [0] * n
-    print(deg)
-
-    for u,v in edges:
-        deg[u] += 1
-        deg[v] += 1
-    return deg
-
-print(deg_graph(3, [[0,1], [0,2], [1,2]]))

26a7c055c5afa9b5f1b592d999ff48cf5efdf6e9
Prateek Rai <110284131+Prateek007rai@users.noreply.github.com>
2026-05-11 23:36:46 +0530

    Update 4 - Product except self.py

--- Array/4 - Product except self.py
+++ Array/4 - Product except self.py
@@ -3,8 +3,10 @@ def product_arr_except_self(arr):
     res = [1] * len(arr)
     prefix = 1
     postfix = 1
-    
 
+    for i in range(len(arr)):
+        res[i] = prefix
+        prefix = prefix * arr[i]
     
     for i in range(len(arr) - 1, -1, -1):
         res[i] = res[i]*postfix

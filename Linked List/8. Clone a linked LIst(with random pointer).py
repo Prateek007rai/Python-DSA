58a4e0ba221a3df3891b25315abf9b0ce2d60399
Prateek Rai <110284131+Prateek007rai@users.noreply.github.com>
2026-05-12 23:33:56 +0530

    Update 8. Clone a linked LIst(with random pointer).py

--- Linked List/8. Clone a linked LIst(with random pointer).py
+++ Linked List/8. Clone a linked LIst(with random pointer).py
@@ -3,8 +3,6 @@
 # Input: Nodes: 1, 2, 3
 # Pointers: (1.next=2, 1.rand=3), (2.next=3, 2.rand=1), (3.next=None, 3.rand=2)
 
-# Output:
-# A completely new set of memory addresses for nodes 1, 2, 3 with the exact same value and pointer relationships as the input.
 
 def clone_LL(head):
     if not head: 

6b7d019563cb1486ff431ed2239e49505c57fbc4
Prateek Rai <110284131+Prateek007rai@users.noreply.github.com>
2026-05-21 23:19:32 +0530

    Update 1. Check if array is a heap.py

--- Heap/1. Check if array is a heap.py
+++ Heap/1. Check if array is a heap.py
@@ -5,17 +5,6 @@
 def is_max_heap(arr):
     n = len(arr)
 
-    for i in range(len(arr)):
-
-        left = 2*i+1
-        right = 2*i+2
-
-        if left < n and arr[i] < arr[left]:
-            return False
-        
-        if right < n and arr[i] < arr[right]:
-            return False
-        
-    return True
+    
 
 print(is_max_heap([10,5,6,2]))

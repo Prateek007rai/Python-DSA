dc1ce22b6d6dcca49384e5e8e71e46cfd8e433ab
Prateek Rai <110284131+Prateek007rai@users.noreply.github.com>
2026-05-22 23:49:24 +0530

    Update 1. Reverse LL.py

--- Linked List/1. Reverse LL.py
+++ Linked List/1. Reverse LL.py
@@ -9,5 +9,11 @@ def reverse_list(head):
     prev = None
     curr = head
     
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
     
+    return prev
     

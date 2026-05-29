01ff85a370253f40688ade9082c7590378d692fc
Prateek Rai <Prateekrai341036@gmail.com>
2026-05-29 22:49:49 +0530

    [Practiced] - Remove nth node from linked list

--- test.py
+++ test.py
@@ -815,3 +815,7 @@ def remove_nth_node(head, n):
         fast = fast.next
 
     while fast.next:
+        slow = slow.next
+        fast = fast.next
+    slow.next = slow.next.next
+    return dummy.next

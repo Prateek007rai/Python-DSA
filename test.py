4e1e5718f7e976d39c8371e87e839328792a199e
Prateek Rai <Prateekrai341036@gmail.com>
2026-06-18 23:26:45 +0530

    [Practiced] Longest palindrome

--- test.py
+++ test.py
@@ -1094,12 +1094,13 @@ def longest_pal(s):
 
     for i in range(len(s)):
         for l,r in [(i, i), (i, i+1)]:
-            while l > 0 and r < len(s) and s[l] == s[r]:
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r-l+1) > len(res):
                     res = s[l: r+1]
-                l += 1
-                r -= 1
+                l -= 1
+                r += 1
     return res
+print(longest_pal('babad'))
 
 # 42. Smallest Window Containing All chars
 

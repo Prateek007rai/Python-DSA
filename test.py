b6298e0b7eacd66ddd98ce1857fc7d3f5dfb619f
Prateek Rai <Prateekrai341036@gmail.com>
2026-06-19 18:50:12 +0530

    [Practiced] Move Zeroes to the end of the array

--- test.py
+++ test.py
@@ -1173,5 +1173,15 @@ print("rotate arr: ", rotate_arr([1,2,3,4,5,6,7], 2))
 
 # 45. Move zeroes to the end of arr
 def move_zeroes(arr):
+    r = len(arr) - 1
+
+    for l in range(len(arr)):
+
+        if arr[l] == 0:
+            arr[l], arr[r] = arr[r], arr[l]
+            r -= 1
     
+    return arr
+
+
         

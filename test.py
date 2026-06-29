daaf0215d97b6c8faf7b126e5b2fff356107a456
Prateek Rai <Prateekrai341036@gmail.com>
2026-06-29 23:52:46 +0530

    [Practiced] - Search Peak element

--- test.py
+++ test.py
@@ -1418,7 +1418,7 @@ def Peak_element(arr):
         if arr[mid] < arr[mid+1]:
             l = mid + 1
         else:
-            # r = mid - 1
+            r = mid - 1
     return l
 
 print(Peak_element([1,2,3,1]))

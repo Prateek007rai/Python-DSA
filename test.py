18b1a3e45565af5f3378f54d0f7ea770754aa740
Prateek Rai <Prateekrai341036@gmail.com>
2026-06-30 23:43:48 +0530

    [Practiced] - Search Peak element

--- test.py
+++ test.py
@@ -1401,9 +1401,9 @@ def search_rotated_arr(arr, target):
                 l = mid + 1
         else:
             if arr[mid] <= target <= arr[r]:
-                l = mid + 1
+                # l = mid + 1
             else:
-                # r = mid - 1
+                r = mid - 1
     
     return None
 

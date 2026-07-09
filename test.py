9d608134815e3deef3ccd6159f2ebdd4cbeff514
Prateek Rai <Prateekrai341036@gmail.com>
2026-07-09 23:19:48 +0530

    [Practiced] merge two sorted arrays without using extraa space

--- test.py
+++ test.py
@@ -1707,4 +1707,22 @@ def count_inversions(arr):
 print(count_inversions([2,4,1,3,5]))
 
 # 64. Merge two sorted arrays without extraa space
+def merge_two_sort_arrs(arr1, arr2):
+    m, n = len(arr1), len(arr2)
+    i, j = m-1, 0
+    
+    while i >= 0 and j < n:
+        if arr1[i] > arr2[j]:
+            arr1[i], arr2[j] = arr2[j], arr1[i]
+            i -= 1
+            j += 1
+    
+    arr1.sort()
+    arr2.sort()
+
+    return arr1, arr2
+
+# 65. Choclate Distribution Problem
+
+# 66. Sort Numbers (odd in descending and even in ascending)
 

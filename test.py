c5adc5498c0c467568d59f85e101c818b01fc111
Prateek Rai <Prateekrai341036@gmail.com>
2026-07-17 23:37:23 +0530

    [practiced] - Sort numbers

--- test.py
+++ test.py
@@ -1508,7 +1508,7 @@ def merge_sort(arr):
     
     mid = len(arr) // 2
     left_arr_sorted = merge_sort(arr[:mid])
-    right_arr_sorted = merge_sort(arr[mid:])
+    # right_arr_sorted = merge_sort(arr[mid:])
     
     return merge(left_arr_sorted, right_arr_sorted)
 

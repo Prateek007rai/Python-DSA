521b6a451642eacad586440206d241a187362e08
Prateek Rai <Prateekrai341036@gmail.com>
2026-07-31 23:45:37 +0530

    [practiced]  - Are these two tree are mirror tree?

--- test.py
+++ test.py
@@ -2097,3 +2097,8 @@ def max_depth(root):
     return 1 + max(left, right)
 
 def is_mirror(t1, t2):
+    if not t1 and not t2:
+        return True
+    if not t1 or not t2:
+        return False
+    return (t1.val == t2.val and is_mirror(t1.left, t2.right) and is_mirror(t1.right, t2.left))

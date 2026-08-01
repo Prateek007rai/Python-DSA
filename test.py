99195c39cdfcc61943f3dadd6f11522fb00f1ea2
Prateek Rai <Prateekrai341036@gmail.com>
2026-08-01 16:40:28 +0530

    [practiced]  - Are these two tree are mirror tree?

--- test.py
+++ test.py
@@ -2103,3 +2103,6 @@ def is_mirror(t1, t2):
     if not t1 or not t2:
         return False
     return (t1.val == t2.val and is_mirror(t1.left, t2.right) and is_mirror(t1.right, t2.left))
+
+# 84. Invert/flip binary tree
+def invert_tree(root):

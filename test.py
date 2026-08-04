001725c211de09f62847fd864ee9c8aef36baf1c
Prateek Rai <Prateekrai341036@gmail.com>
2026-08-04 22:23:20 +0530

    save

--- test.py
+++ test.py
@@ -2105,7 +2105,7 @@ def is_mirror(t1, t2):
     return (t1.val == t2.val and is_mirror(t1.left, t2.right) and is_mirror(t1.right, t2.left))
 
 # 84. Invert/flip binary tree
-# def invert_tree(root):
+def invert_tree(root):
     if not root:
         return None
     root.left, root.right = root.right, root.left

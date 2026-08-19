6ce83d8f6d4033b8e98c9073ae077070de6dbc04
Prateek Rai <Prateekrai341036@gmail.com>
2026-08-19 12:44:39 +0530

    Practiced: diameter of root

--- test.py
+++ test.py
@@ -2162,5 +2162,21 @@ def level_order(root):
 # 87. Diameter of binary tree
 def diameter(root):
     max_dia = 0
+    def dfs(node):
+        nonlocal max_dia
+
+        if not node:
+            return 0
+
+        left = dfs(node.left)
+        right = dfs(node.right)
+
+        max_dia = max(max_dia, left+right)
+
+        return 1 + max(left, right)
+
+    dfs(root)
+    return max_dia
+    
 
 # 88. Serialize and deserialize

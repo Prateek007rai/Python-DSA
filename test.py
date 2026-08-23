f28c9a4a47e4df5e937e85445ed88f6c9c0f0522
Prateek Rai <Prateekrai341036@gmail.com>
2026-08-23 17:26:00 +0530

    Practiced: 2nd highest number

--- test.py
+++ test.py
@@ -472,6 +472,17 @@ print("Check is valid paranthesis: ", is_valid_paranthesis_1("()[]{}"))
 # Practice
 # 2nd highest number in array 
 
+def find_num(arr):
+    first = second = float('-inf')
+
+    for num in arr:
+        if num > first: 
+            second = first
+            first = num
+        elif first > num > second:
+            second = num
+    return second
+print(find_num([34,45,53,21,24,64,54]))
 
 
 # START A NEW DAY WITH A NEW THOUGHT

ac98255882a371a6996c343addb32bf5d059c49a
Prateek Rai <Prateekrai341036@gmail.com>
2026-08-23 17:27:05 +0530

    Practiced: string compression

--- test.py
+++ test.py
@@ -486,6 +486,21 @@ print(find_num([34,45,53,21,24,64,54]))
 
 
 # String compression
+def compress(str1):
+    res = ''
+    count = 1
+
+    for i in range(1, len(str1)):
+        if str1[i] == str1[i-1]:
+            count += 1
+        else:
+            res = res + str1[i-1] + str(count)
+            count = 1
+    
+    res = res + str1[-1] + str(count) 
+    return res
+
+print(compress("aaaaaa"))
 
 
 # START A NEW DAY WITH A NEW THOUGHT
